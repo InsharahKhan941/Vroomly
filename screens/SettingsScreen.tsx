f8eee8794ad8f1501bb5844e98a55d4fa262a52c
haniyakk <haniyakhankakar8@gmail.com>
2025-11-22 10:26:42 +0500

    Updated settings screen

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -51,11 +51,6 @@ const SettingsScreen: React.FC = () => {
                     label="Change Password"
                     onClick={() => setScreen(Screen.CHANGE_PASSWORD)}
                 />
-                <SettingsItem
-                    icon={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>}
-                    label="Delete Account"
-                    onClick={handleDeleteAccount}
-                />
                  <SettingsItem
                     icon={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>}
                     label="Logout"
